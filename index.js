d538c7a502c3868f476b495d5707471baed6dfab
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-08 09:59:20 +0530

    add js-ascending order 3 numbers

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let a = 15, b = 9, c = 2;
+if (a < b && a < c) {
+    if (b < c) {
+        console.log(a + ", " + b + ", " + c);
+    }
+    else {
+        console.log(a + ", " + c + ", " + b);
+    }
+}
+else if (b < a && b < c) {
+    if (a < c) {
+        console.log(b + ", " + a + ", " + c);
+    }
+    else {
+        console.log(b + ", " + c + ", " + a);
+    }
+}
+else if (c < a && c < b) {
+    if (a < b) {
+        console.log(c + ", " + a + ", " + b);
+    }
+    else {
+        console.log(c + ", " + b + ", " + a);
+    }
+}
